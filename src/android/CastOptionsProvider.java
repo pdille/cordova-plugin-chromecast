dc8a9f79199aad965ff481f8be5a6ff0aa113c3c
Lindsay-Needs-Sleep <lindsay-needs-sleep@outlook.com>
2020-11-01 22:34:00 -0700

    How to do basic UI customization for cast push notification

--- src/android/CastOptionsProvider.java
+++ src/android/CastOptionsProvider.java
@@ -1,10 +1,14 @@
 package acidhax.cordova.chromecast;
 
+import java.util.Arrays;
 import java.util.List;
 
 import com.google.android.gms.cast.framework.CastOptions;
 import com.google.android.gms.cast.framework.OptionsProvider;
 import com.google.android.gms.cast.framework.SessionProvider;
+import com.google.android.gms.cast.framework.media.CastMediaOptions;
+import com.google.android.gms.cast.framework.media.MediaIntentReceiver;
+import com.google.android.gms.cast.framework.media.NotificationOptions;
 
 import android.content.Context;
 
@@ -23,8 +27,18 @@ public final class CastOptionsProvider implements OptionsProvider {
 
     @Override
     public CastOptions getCastOptions(Context context) {
+        NotificationOptions notificationOptions = new NotificationOptions.Builder()
+                .setActions(Arrays.asList(
+                        MediaIntentReceiver.ACTION_TOGGLE_PLAYBACK,
+                        MediaIntentReceiver.ACTION_STOP_CASTING
+                ), new int[] { 0, 1 })
+                .build();
+        CastMediaOptions mediaOptions = new CastMediaOptions.Builder()
+                .setNotificationOptions(notificationOptions)
+                .build();
         return new CastOptions.Builder()
                 .setReceiverApplicationId(appId)
+                .setCastMediaOptions(mediaOptions)
                 .build();
     }
     @Override
